885c4c7bf93d586f8476f4ba9f81d1a9bbf62197
unknown <ozelot@ctemplar.com>
2021-12-23 13:18:29 +0100

    Created sidebar and content component

---
+++ components/Content.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="mt-5">
+        <main class="p-2">
+            <h1 class="text-center">Wikipedia mcMMO</h1>
+            <article v-html="content"></article>
+        </main>
+        <footer class="m-0 pb-1">
+            <p class="text-center text-muted">Informacje pochodzą z <a href="https://mcmmo.fandom.com/wiki/McMMO_Wiki">McMMO Wiki</a> i są rozpowszechniane na licencji <b>CC-BY-SA</b>.</p>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        content() {
+            const article = this.$route.path == "/" ? this.articles["_"] : this.articles[this.$route.params.pathMatch];
+            return article.replaceAll("{", "<span class='badge badge-guide'>").replaceAll("}", "</span>");
+        }
+    },
+    data() {
+        return {
+            articles: {}
+        }
+    },
+    created() {
+        this.importAll(require.context("../articles/", true, /\.md$/));
+    },
+    methods: {
+        importAll(r) {
+            r.keys().forEach(key => (this.articles[key.replace("./", "").replace(".md", "")] = r(key).default));
+        }
+    }
+}
+</script>
